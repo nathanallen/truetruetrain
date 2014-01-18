ca84bf9d1cbb8b042478ba72185ea7c1a8684926
nathanallen <thenathanator@gmail.com>
2014-01-17 19:01:50 -0800

    more refactoring

--- trains.rb
+++ trains.rb
@@ -1,17 +1,20 @@
 
-TRAIN_HASH = Hash.new()
+TRAIN_ROUTES = Hash.new("NO SUCH ROUTE")
 
-def total_distance(*stops)
-  output = 0
-  stops.each_with_index do |current_stop, i|
-    next_stop = stops[i+1]
-    if next_stop
-      route_value = TRAIN_HASH[current_stop][next_stop]
-      return route_value if route_value.is_a? String # ie "NO SUCH ROUTE" error message
-      output += route_value
-    end
+def distance_along_route(*stops)
+  distance = 0
+  legs = stops.length-1
+  legs.times do |i|
+    stations = stops[i..i+1]
+    output = distance_between(*stations)
+    return output if output == "NO SUCH ROUTE"
+    distance += output
   end
-  output
+  distance
+end
+
+def distance_between(current_station, next_station)
+  TRAIN_ROUTES[current_station][next_station]
 end
 
 
@@ -23,27 +26,25 @@ def number_of_trips(origin, destination, *opts)
   routes.count
 end
 
-def find_routes(trail, destination, max, min) #depth first search
+def find_routes(route, destination, max, min) #depth first search
   trails = []
-  #if trail.length < max
-    connections = TRAIN_HASH[trail.first.last]
-    connections.each_pair do |stop, dist|
-      current_trail = trail.first.dup << stop
-      distance = trail.last + dist
-      current_thing = [current_trail, distance]
-      if stop == destination && current_trail.length >= min
-        trails << current_thing
-      elsif current_trail.length < max ## move this
-        more_trails = find_routes(current_thing, destination, max, min)
-        trails += more_trails
-      end 
-    end
-  #end
+  connections = TRAIN_ROUTES[route.first.last]
+  connections.each_pair do |stop, dist|
+    trail_so_far = route.first.dup << stop
+    distance_so_far = route.last + dist
+    current_route = [trail_so_far, distance_so_far]
+    if stop == destination && trail_so_far.length >= min
+      trails << current_route
+    elsif trail_so_far.length < max
+      more_trails = find_routes(current_route, destination, max, min)
+      trails += more_trails
+    end 
+  end
   trails
 end
 
 def default_find_routes(origin, destination)
-  max = TRAIN_HASH.keys.length
+  max = TRAIN_ROUTES.keys.length
   min = 0
   trail = [[origin], 0]
   routes = find_routes(trail, destination, max, min)
@@ -84,7 +85,7 @@ end
 # def routes_by_stop_count()
 # end
 
-# def routes_by_total_distance()
+# def routes_by_distance_along_route()
 # end
 
 # def shortest_route()
@@ -95,12 +96,12 @@ end
 
 
 def update_graph(origin, destination, distance)
-  if TRAIN_HASH[origin]
-    TRAIN_HASH[origin][destination] = distance.to_i #
+  if TRAIN_ROUTES[origin] != TRAIN_ROUTES.default
+    TRAIN_ROUTES[origin][destination] = distance.to_i #
   else
     new_hash = Hash.new("NO SUCH ROUTE")
     new_hash[destination] = distance.to_i #
-    TRAIN_HASH[origin] = new_hash
+    TRAIN_ROUTES[origin] = new_hash
   end
 end
 
@@ -112,7 +113,7 @@ def load_graph(graph_strings)
     update_graph(*route_string.split(''))
   end
   # p "LOADING GRAPH..."
-  # p TRAIN_HASH
+  # p TRAIN_ROUTES
   # p "+++++++++++++"
 end
 
@@ -125,23 +126,23 @@ load_graph(graph_strings)
 # 3. The distance of the route A-D-C.
 # 4. The distance of the route A-E-B-C-D.
 # 5. The distance of the route A-E-D.
-p "#1: #{total_distance('A','B','C') == 9}"
-p "#2: #{total_distance('A','D') == 5}"
-p "#3: #{total_distance('A','D','C') == 13}"
-p "#4: #{total_distance('A','E','B','C','D') == 22}"
-p "#5: #{total_distance('A','E','D') == 'NO SUCH ROUTE'}"
+p "#1: #{distance_along_route('A','B','C') == 9}"
+p "#2: #{distance_along_route('A','D') == 5}"
+p "#3: #{distance_along_route('A','D','C') == 13}"
+p "#4: #{distance_along_route('A','E','B','C','D') == 22}"
+p "#5: #{distance_along_route('A','E','D') == 'NO SUCH ROUTE'}"
 
-# 6. The number of trips starting at C and ending at C with a maximum of 3 stops.  In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops).
-# 7. The number of trips starting at A and ending at C with exactly 4 stops.  In the sample data below, there are three such trips: A to C (via B,C,D); A to C (via D,C,D); and A to C (via D,E,B).
-p "#6: #{number_of_trips('C','C',3) == 2}"
-p "#7: #{number_of_trips('A','C',4,4) == 3}"
-#p "ABCDC, ADCDC, ADEBC"
+# # 6. The number of trips starting at C and ending at C with a maximum of 3 stops.  In the sample data below, there are two such trips: C-D-C (2 stops). and C-E-B-C (3 stops).
+# # 7. The number of trips starting at A and ending at C with exactly 4 stops.  In the sample data below, there are three such trips: A to C (via B,C,D); A to C (via D,C,D); and A to C (via D,E,B).
+# p "#6: #{number_of_trips('C','C',3) == 2}"
+# p "#7: #{number_of_trips('A','C',4,4) == 3}"
+# #p "ABCDC, ADCDC, ADEBC"
 
-# 8. The length of the shortest route (in terms of distance to travel) from A to C.
-# 9. The length of the shortest route (in terms of distance to travel) from B to B.
-p "#8: #{length_of_shortest_route('A','C') == 9}"
-p "#9: #{length_of_shortest_route('B','B') == 9}"
+# # 8. The length of the shortest route (in terms of distance to travel) from A to C.
+# # 9. The length of the shortest route (in terms of distance to travel) from B to B.
+# p "#8: #{length_of_shortest_route('A','C') == 9}"
+# p "#9: #{length_of_shortest_route('B','B') == 9}"
 
-# 10. The number of different routes from C to C with a distance of less than 30.  In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC.
-p "#10: #{number_of_unique_routes('C','C',30) == 7}"
-#p "CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC"
+# # 10. The number of different routes from C to C with a distance of less than 30.  In the sample data, the trips are: CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC.
+# p "#10: #{number_of_unique_routes('C','C',30) == 7}"
+# #p "CDC, CEBC, CEBCDC, CDCEBC, CDEBC, CEBCEBC, CEBCEBCEBC"
